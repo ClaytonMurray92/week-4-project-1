d43470195dd5b113443b4fb9c9ecfa762a4d8a67
ClaytonMurray92 <clamurr@okstate.edu>
2024-10-20 16:48:15 -0400

    Add files via upload

---
+++ Week 4 Project 1.py
@@ -0,0 +1,7 @@
+first = int(input("Enter the first side: "))
+second = int(input("Enter the second side: "))
+third = int(input("Enter the second side: "))
+if first == second == third:
+    print("The triangle is equilateral")
+else:
+    print("The triangle is not equilateral")
